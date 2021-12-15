3e8503b9271303772af609ff61dcd151ac0f6454
Hain Thit <hainlthit@gmail.com>
2021-12-14 19:37:48 -0500

    commit tree 1656e6206643d4424a71d95f83df7c91352e6d54

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity='roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun('bathe my dog!')
+
+function mondayWork(location='go to the office'){
+    return `This Monday, I will ${location}.`
+}
+mondayWork('work from home.')
+
+
+function wrapAdjective(wrapper="*") {
+    const part1 = "You are"
+    return function innerFunction(job="a hard worker") {
+return `${part1} ${wrapper}${job}${wrapper}!`
+    };
+}
+wrapAdjective('||')('a dedicated programmer')
